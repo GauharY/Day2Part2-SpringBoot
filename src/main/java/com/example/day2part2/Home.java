75dde820d30ee419e075a043561d8007acd0dafd
Gauhar Yadav <gauharyadav@gmail.com>
2026-01-19 15:24:43 +0530

    using arraylist

--- src/main/java/com/example/day2part2/Home.java
+++ src/main/java/com/example/day2part2/Home.java
@@ -3,6 +3,9 @@ package com.example.day2part2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class Home {
 //    @GetMapping("/home")
@@ -13,5 +16,14 @@ public class Home {
     public StudentModel getStudent(){
         StudentModel student = new StudentModel(1, "gauhar", "gauharyadav@email.com");
         return student;
+    }
+    @GetMapping("/listings")
+    public List<StudentModel> listing(){
+        List<StudentModel> students = new ArrayList<>();
+        students.add(new StudentModel(2, "krishna", "krishna@gmail.com"));
+        students.add(new StudentModel(3, "tejas", "tejas@gmail.com"));
+        students.add(new StudentModel(4, "aditya", "aditya@gmail.com"));
+        students.add(new StudentModel(5, "faguni", "faguni@gmail.com"));
+        return students;
     }
 }
